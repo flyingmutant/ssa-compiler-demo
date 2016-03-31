1486f7ddc2015fee6756b3054d8363735b9d3583
Gregory Petrosyan <gregory.petrosyan@gmail.com>
2016-03-31 22:00:57 +0300

    +1

--- compiler-demo/c/pa2s.c
+++ compiler-demo/c/pa2s.c
@@ -25,12 +25,11 @@ uint8_t r[ITEMS] = { [0 ... (ITEMS - 1)] = 'r' };
 uint8_t g[ITEMS] = { [0 ... (ITEMS - 1)] = 'g' };
 uint8_t b[ITEMS] = { [0 ... (ITEMS - 1)] = 'b' };
 uint8_t a[ITEMS] = { [0 ... (ITEMS - 1)] = 'a' };
+
 rgba_pa pa = { r, g, b, a };
 
 static void pa2s(const rgba_pa *restrict pa, rgba_s *restrict s, size_t n)
 {
-	n *= 8;
-
 	for (size_t x = 0; x < n; x++) {
 		s[x].r = pa->r[x];
 		s[x].g = pa->g[x];
@@ -43,7 +42,7 @@ int main(void)
 {
 	for (size_t x = 0; x < (1000 * 1000); x++)
 		for (size_t y = 0; y < 64; y++)
-			pa2s(&pa, s, ITEMS/8);
+			pa2s(&pa, s, ITEMS);
 
 	return 0;
 }
